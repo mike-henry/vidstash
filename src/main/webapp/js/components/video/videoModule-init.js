1255a60cda4eacc90b14fac82a1cd116d2975a80
mike-henry <michael.henry@atos.net>
2015-08-01 09:08:59 +0100

    Added Modulization with Angular and RequireJS  Each module needs a module-init.js to avoid cross dependency and enable controllers services etc to append to the module

---
+++ src/main/webapp/js/components/video/videoModule-init.js
@@ -0,0 +1,14 @@
+
+
+
+define(['require','angular'],function(require,_a){
+
+	
+	var module = angular.module('com.spx.vidstash.video',[]);
+
+	
+	
+	return module;
+	
+	
+});
---
+++ src/main/webapp/js/components/video/videoModule.js
@@ -0,0 +1,14 @@
+
+
+
+define(['require','angular','./model/Video','./ManageVideo'],function(require,_a,videoModel,manageVideo){
+
+	
+	var module = angular.module('com.spx.vidstash.video');
+
+	
+	
+	return module;
+	
+	
+});
